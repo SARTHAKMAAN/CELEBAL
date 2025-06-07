f554f250ec48f8d003b911c76f34c2716bd06ded
SARTHAKMAAN <sarthakmaan3590@gmail.com>
2025-06-07 16:19:26 +0530

    arithmetic operations

---
+++ Arithmetic_operator.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    print("Enter the two Numbers for arithmatic operations: ")
+    a = int(input())
+    b = int(input())
+    
+    #addition of a & b
+    print(a+b)
+   
+    #subtraction of a & b
+    print(a-b)
+
+    #multiplication of a & b
+    print(a*b)
+
+    
